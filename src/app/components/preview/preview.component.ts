1943d16c5e45607551f58dc5d507b8f30f161ee4
divyam0212 <45897098+divyam0212@users.noreply.github.com>
2019-02-27 19:09:00 +0530

    Update preview.component.ts

--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -8,27 +8,90 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class PreviewComponent implements OnInit {
 
-  checkId:any;
-  hero:any={
+  checkId: any;
+  contentVisible: boolean = true;
+  hero: any = {
+    "checkId": 1,
+    "checkDescription": "<b>Hello World</b><br> &nbsp; &nbsp; My User Story is to search and add existing checks. <br>",
+    "checkDuration": 2,
+    "checkTitle": "Sum of two integers",
+    "checkType": "PRACTICE CHECK",
+    "checkInput": "a=3 b=5",
+    "checkOutput": "Sum is: 8"
+  }
+
+  /* hero:any={
     "checkId":1,
     "checkDescription":"<b>Hello World</b><br> &nbsp; &nbsp; My User Story is to search and add existing checks. <br>",
     "checkDuration":2,
     "checkTitle": "Sum of two integers",
-    "checkType":"PRACTICE CHECK"
-  }
-  constructor(private route:ActivatedRoute,private router:Router) { }
+    "checkType":"PRACTICE CHECK",
+    "checkOutput":"Sum is: 8"
+  } */
+
+  /* hero:any={
+    "checkId":1,
+    "checkDescription":"<b>Hello World</b><br> &nbsp; &nbsp; My User Story is to search and add existing checks. <br>",
+    "checkDuration":2,
+    "checkTitle": "Sum of two integers",
+    "checkType":"PRACTICE CHECK",
+    "checkInput":"a=3 b=5"
+  } */
+
+
+  /* hero:any={
+    "checkId":1,
+    "checkDuration":2,
+    "checkTitle": "Sum of two integers",
+    "checkType":"PRACTICE CHECK",
+    "checkInput":"a=3 b=5",
+    "checkOutput":"Sum is: 8"
+  } */
+
+  /* hero:any={
+    "checkId":1,
+    "checkDuration":2,
+    "checkInput":"a=3 b=5",
+    "checkOutput":"Sum is: 8"
+  } */
+
+
+  scores: any = [
+    { "checkCondition":  "you need to have commands with code",  "mark": 20 },
+    { "checkCondition":  "you need to have proper spacing in code",  "mark": 10 }
+  ];
+  /* scores:any=[];  */
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.checkId=this.route.snapshot.paramMap.get('checkId');
-    alert("CHECK ID:--->"+this.checkId);
+    this.checkId = this.route.snapshot.paramMap.get('checkId');
+
+    (function () {
+      'use strict';
+      window.addEventListener('load',  function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var  forms  =  document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var  validation  =  Array.prototype.filter.call(forms,  function (form) {
+          form.addEventListener('submit',  function (event) {
+            if  (form.checkValidity()  ===  false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          },  false);
+        });
+      },  false);
+    })();
   }
-  backFn(){
+  backFn() {
     alert("GOING BACK TO SEARCH");
     this.router.navigate(['/acco']);
   }
 
-  addCheck(hero:any){
-    alert("CHECK ADDED: -------> "+JSON.stringify(hero));
+  addCheck(hero: any) {
+    alert("CHECK ADDED: -------> " + JSON.stringify(hero));
   }
 
 }
